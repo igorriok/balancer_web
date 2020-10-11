8cdba71f76a012c13841ca438759ad2ac80e2f5c
ISOLONARI <igor.solonari@orange.md>
2020-10-12 00:22:06 +0300

    Login page

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
+const sendCredentials = (event: any) => {
+	console.log(event);
+	event.preventDefault();
+}
+
 
 export default function LoginPage() {
 	
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 	
+	const handleUsername = (event: any) => {
+		setUsername(event.target.value)
+	}
+	
+	const handlePassword = (event: any) => {
+		setPassword(event.target.value);
+	}
 
 	return (
-		<form method="post">
+		<form onSubmit={sendCredentials} autoComplete="on">
 			<div className="container">
-				<label htmlFor="uname"><b>Username</b></label>
-				<input type="text" placeholder="Enter Username" name="uname" required/>
-					
-				<label htmlFor="psw"><b>Password</b></label>
-				<input type="password" placeholder="Enter Password" name="psw" required/>
+				<label htmlFor="uname">
+					<b>
+						Username
+					</b>
+				</label>
+				<input
+					type="text"
+					placeholder="Enter Username"
+					name="uname"
+					required
+					value={username}
+					onChange={handleUsername}
+				/>
+			
+				<label htmlFor="psw">
+					<b>
+						Password
+					</b>
+				</label>
+				<input
+					type="password"
+					placeholder="Enter Password"
+					name="psw"
+					required
+					value={password}
+					onChange={handlePassword}
+				/>
 				
 				<button type="submit">
 					Login
@@ -20,6 +58,7 @@ export default function LoginPage() {
 				<label>
 					<input type="checkbox" checked={true} name="remember"/> Remember me
 				</label>
+				
 			</div>
 		</form>
 	);
